ff5e56a755caaae60bb2cb01ec2c6071e5363dec
Dawid Kopys <dewke17@gmail.com>
2018-06-22 22:30:34 +0200

    s11w144: connect widgets to functions

--- tkinter_gui/tkinter_gui.py
+++ tkinter_gui/tkinter_gui.py
@@ -1,11 +1,17 @@
 from tkinter import *
 
+def km_to_miles():
+    print(e1_value.get())
+    miles = float(e1_value.get())*0.621371192
+    t1.insert(END, miles)
+
 window = Tk()
 
-b1 = Button(window, text="Execute")
+b1 = Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0)
 
-e1 = Entry(window)
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
 t1 = Text(window, height=2, width=20)
