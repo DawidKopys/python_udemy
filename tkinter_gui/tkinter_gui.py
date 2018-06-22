5513f7985b1eaa1393bea2e507f3fdf809c4c00d
Dawid Kopys <dewke17@gmail.com>
2018-06-22 21:59:33 +0200

    s11w143: tkinter gui introduction

--- tkinter_gui/tkinter_gui.py
+++ tkinter_gui/tkinter_gui.py
@@ -1,1 +1,14 @@
-# content
+from tkinter import *
+
+window = Tk()
+
+b1 = Button(window, text="Execute")
+b1.grid(row=0, column=0)
+
+e1 = Entry(window)
+e1.grid(row=0, column=1)
+
+t1 = Text(window, height=2, width=20)
+t1.grid(row=1,column=2)
+
+window.mainloop()
